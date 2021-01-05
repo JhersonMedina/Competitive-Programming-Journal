8a08a4add3087fc912f5dd5076e68d2ff4db2a5b
Jherson Medina <38172045+JhersonMedina@users.noreply.github.com>
2021-01-05 17:00:48 -0500

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* * * *   * * *   	* * * */
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+/* * * *   * * *   	* * * */
+/* *
+ *
+ * I love Ylva Eriksson
+ *
+ * */
+int main(){
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	return 0;
+}
+
