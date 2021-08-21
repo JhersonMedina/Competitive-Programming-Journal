5d76b0e93fb89560b15cca3dbbd4cc56f52478bc
Jherson Medina <38172045+JhersonMedina@users.noreply.github.com>
2021-08-21 12:19:06 -0500

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -5,8 +5,19 @@ using namespace std;
 #define fst first 
 #define snd second 
 #define pb push_back 
-#define ALL(x) x.begin(), x.end()
-#define SZ(x) x.size()
+#define rsz resize
+#define ins insert
+#define ers erase
+#define lb lower_bound
+#define ub upper_bound
+#define ft front()
+#define bk back()
+#define bg(x) x.begin()
+#define ed(x) x.end()
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sz(x) int((x).size())
+#define compress(x) sort(all(x)); x.rsz(unique(all(x)) - bg(x))
 #define ms(a, v) memset((a), (v), sizeof (a))
 typedef long long ll;
 typedef pair<int, int> pii;
