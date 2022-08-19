8ad35318a77056bf5d1ed0ee3130a2dba79259e7
Jherson Medina <38172045+JhersonMedina@users.noreply.github.com>
2022-08-19 11:43:26 -0500

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,44 +1,31 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+
 using namespace std;
-/* * * *   * * *   	* * * */
-#define mp make_pair
-#define fst first 
-#define snd second 
-#define pb push_back 
-#define rsz resize
-#define agn assign
-#define ins insert
-#define ers erase
-#define lb lower_bound
-#define ub upper_bound
-#define ft front()
-#define bk back()
-#define bg(x) x.begin()
-#define ed(x) x.end()
-#define all(x) bg(x), end(x)
-#define rall(x) x.rbegin(), x.rend()
-#define sz(x) int((x).size())
-#define compress(x) sort(all(x)), x.rsz(unique(all(x)) - bg(x))
-#define ms(a, v) memset((a), (v), sizeof (a))
-#define deb(x) cout << #x << ": " << x << endl
-#define forn(i,n) for (int i = 0; i < (int)(n); ++i)
-#define fore(i, a, b) for (int i = (int)(a); i <= (int)(b); ++i)
-#define dforn(i,n) for (int i = (int)(n) - 1; i >= 0; --i)
-#define dfore(i, a, b) for (int i = (int)(b); i >= a; --i)
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector <int> vi;
-typedef vector <pii> vpi;
-typedef pair<ll, ll> pll;
-typedef vector <ll> vl;
-typedef vector <pll> vpl;
-/* * * *   * * *   	* * * */
 /* *
  *
  * Too many mind, no mind.
  *
  * */
-int main(){
+int main() {
 	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 	//freopen("input.txt", "r", stdin);
 	//freopen("output.txt", "w", stdout);
