6f1321199952c54dbd2d88311e4b306df1cfcd80
Jherson Medina <38172045+JhersonMedina@users.noreply.github.com>
2023-09-10 17:22:18 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,9 +1,9 @@
-$VAR1 = Get-Content -path ./output.txt
-$VAR2 = Get-Content -path ./outbrute.txt
 ./gen.exe
 ./d.exe
 ./brute.exe
 
+$VAR1 = Get-Content -path ./output.txt
+$VAR2 = Get-Content -path ./outbrute.txt
  while ($VAR1 -eq $VAR2) {
     write-host "OK"
     ./gen.exe
@@ -16,7 +16,5 @@ $VAR2 = Get-Content -path ./outbrute.txt
 
 write-host "Wrong Answer, test case: "
 cat input.txt
-write-host "Answer: $VAR1"
-write-host "output: $VAR2"
-
-
+write-host "Answer: $VAR2"
+write-host "output: $VAR1"
